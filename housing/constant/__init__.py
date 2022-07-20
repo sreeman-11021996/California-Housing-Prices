71a1606e9e043fecca945f78d948dd59d2e6464f
sreeman-11021996 <sreemanbitsmech@gmail.com>
2022-07-20 18:37:33 +0530

    data_transformation.py

--- housing/constant/__init__.py
+++ housing/constant/__init__.py
@@ -53,6 +53,18 @@ DATA_TRANSFORMATION_TRANSFORMED_TEST_DIR_KEY = "transformed_test_dir"
 DATA_TRANSFORMATION_PREPROCESSING_DIR_KEY = "preprocessing_dir"
 DATA_TRANSFORMATION_PREPROCESSING_OBJECT_FILE_NAME_KEY = "preprocessed_object_file_name"
 
+COLUMN_TOTAL_ROOMS = "total_rooms"
+COLUMN_POPULATION = "population"
+COLUMN_HOUSEHOLDS = "households"
+COLUMN_TOTAL_BEDROOM = "total_bedrooms"
+DATASET_SCHEMA_COLUMNS_KEY=  "columns"
+
+NUMERICAL_COLUMN_KEY="numerical_columns"
+CATEGORICAL_COLUMN_KEY = "categorical_columns"
+
+TARGET_COLUMN_KEY="target_column"
+
+
 # Model Trainer related variable
 
 MODEL_TRAINER_CONFIG_KEY = "model_trainer_config"
